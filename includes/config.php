4ad010eeaed395e3b50f8c2b667b4119dc1600cb
vignesh <vigneshshivan6456@gmail.com>
2023-02-23 11:32:31 +0530

    Header Changed

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Welcome to Vick Vision',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [

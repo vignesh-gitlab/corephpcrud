780c107c39a1c8781678fc6bfb7f26f24d497e05
vignesh <vigneshshivan6456@gmail.com>
2023-03-11 08:51:29 +0000

    background images and docker-compose file added

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+    $servername="docker-sql_db_1";
+    $user_name="root";
+    $password="root";
+    $database="test";
+    $conn=mysqli_connect($servername,$user_name,$password,$database);
+?>
